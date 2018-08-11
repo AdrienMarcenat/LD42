24c9aaa0fde4a7354d64b6475f150a516b3a9dcc
Adrien Marcenat <adrien.marcenat@telecom-paristech.fr>
2018-08-11 18:41:10 +0200

    add command number UI

---
+++ LudumDare42/Assets/Scripts/UI/GameGUI/GameGUI.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameGUI : MonoBehaviour
+{
+    [SerializeField] private Text m_CommandNumber;
+
+    private void Update ()
+    {
+        m_CommandNumber.text = "Command Number: " + CommandStackProxy.Get ().GetNumberOfCommand ();
+    }
+
+}
