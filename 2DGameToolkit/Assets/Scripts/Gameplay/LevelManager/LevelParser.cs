1dab34c4168d71d3e79b28a1d0cf5d3042380b15
Gunjin <gunjin.udval@telecom-sudparis.eu>
2018-08-11 13:35:55 +0200

    parser

---
+++ 2DGameToolkit/Assets/Scripts/Gameplay/LevelManager/LevelParser.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using System.IO;
+
+
+public class LevelParser
+{
+
+    protected void ReadFile(string path, string filename)
+    {
+
+        string line;
+
+        // Read the file and display it line by line.  
+        System.IO.StreamReader file =
+            new System.IO.StreamReader(AppendPathSeparator(path) + filename);
+        while ((line = file.ReadLine()) != null)
+        {
+            string[] words = line.Split(' ');
+
+            ObjectType objectType = (ObjectType)Enum.Parse(typeof(ObjectType), (String)words.GetValue(0), true);
+            int x = Int32.Parse((String)words.GetValue(1));
+            int y = Int32.Parse((String)words.GetValue(2));
+
+            switch (objectType)
+            {
+                case ObjectType.Tile:
+                    TileType tileType = (TileType)Enum.Parse(typeof(TileType), (String)words.GetValue(3), true);
+                    Tile tile = new Tile(x, y, tileType);
+                    MapManagerProxy.Get().AddTile(tile);
+                    break;
+                case ObjectType.Truck:
+                    Orientation orientation = (Orientation)Enum.Parse(typeof(Orientation), (String)words.GetValue(3), true);
+                    Truck truck = new Truck(x, y, orientation);
+                    MapManagerProxy.Get().SetTruck(truck);
+                    break;
+                case ObjectType.Goal:
+                    int order = Int32.Parse((String)words.GetValue(3));
+                    Goal goal = new Goal(x, y, order);
+                    MapManagerProxy.Get().AddGoal(goal);
+                    break;
+                case ObjectType.Bin:
+                    int orderBin = Int32.Parse((String)words.GetValue(3));
+                    Bin bin = new Bin(x, y, orderBin);
+                    MapManagerProxy.Get().AddBin(bin);
+                    break;
+            }
+        } 
+
+        file.Close();
+    }
+
+    public string AppendPathSeparator(string filepath)
+    {
+        if (!filepath.EndsWith(@"\"))
+            filepath += @"\";
+
+        return filepath;
+    }
+}
--- 2DGameToolkit/Assets/Scripts/Gameplay/MapSystem/MapManager.cs
+++ 2DGameToolkit/Assets/Scripts/Gameplay/MapSystem/MapManager.cs
@@ -1,40 +1,97 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-enum TileType { Normal, LeftOnly, RightOnly, UpOnly, DownOnly };
-enum ObjectType { Truck, Bin, Goal };
-enum Orientation { Left, Right, Up, Down };
+public enum TileType { Normal, LeftOnly, RightOnly, UpOnly, DownOnly };
+public enum ObjectType { Tile, Truck, Bin, Goal };
+public enum Orientation { Left, Right, Up, Down };
 
 public class MapManager
 {
-    Tile[] tiles;
-    Truck truck;
-    Bin bin;
-    Goal[] goals;
+    List<Tile> tiles { get; set; }
+    Truck truck { get; set; }
+    List<Bin> bins { get; set; }
+    List<Goal> goals { get; set; }
+
+    public MapManager()
+    {
+
+    }
+
+    public void AddTile(Tile tile)
+    {
+        this.tiles.Add(tile);
+    }
+
+    public void SetTruck(Truck truck)
+    {
+        this.truck = truck;
+    }
+
+    public void AddBin(Bin bin)
+    {
+        this.bins.Add(bin);
+    }
+
+    public void AddGoal(Goal goal)
+    {
+        this.goals.Add(goal);
+    }
 }
 
 public class Tile
 {
     int[,] position;
     TileType tileType;
+    TileObject tileObject;
+
+    public Tile (int x, int y, TileType tileType)
+    {
+        this.position.SetValue(x, 0);
+        this.position.SetValue(y, 1);
+        this.tileType = tileType;
+    }
 }
 
-public class Truck
+public class TileObject { }
+
+public class Truck : TileObject
 {
     int[,] position;
     Orientation orientation;
+
+    public Truck (int x, int y, Orientation orientation)
+    {
+        this.position.SetValue(x, 0);
+        this.position.SetValue(y, 1);
+        this.orientation = orientation;
+    }
 }
 
-public class Bin
+public class Bin : TileObject
 {
     int[,] position;
     int order;
+
+    public Bin (int x, int y, int order)
+    {
+        this.position.SetValue(x, 0);
+        this.position.SetValue(y, 1);
+        this.order = order;
+    }
 }
 
-public class Goal
+public class Goal : TileObject
 {
     int[,] position;
     int order;
+
+    public Goal (int x, int y, int order)
+    {
+        this.position.SetValue(x, 0);
+        this.position.SetValue(y, 1);
+        this.order = order;
+    }
 }
 
 public class MapManagerProxy : UniqueProxy<MapManager>
