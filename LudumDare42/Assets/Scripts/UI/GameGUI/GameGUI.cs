865bae13bcb2411b00aef11a1ba5aad570f293d4
Adrien Marcenat <adrien.marcenat@telecom-paristech.fr>
2018-08-13 15:35:21 +0200

    add game GUI

--- LudumDare42/Assets/Scripts/UI/GameGUI/GameGUI.cs
+++ LudumDare42/Assets/Scripts/UI/GameGUI/GameGUI.cs
@@ -3,13 +3,20 @@ using UnityEngine.UI;
 
 public class GameGUI : MonoBehaviour
 {
-    [SerializeField] private Text m_CommandNumber;
-    [SerializeField] private Text m_BinNumber;
+    [SerializeField] private Text m_BestNumber;
+    [SerializeField] private Text m_MoveNumber;
+    [SerializeField] private Text m_LevelName;
+
+    private void Start ()
+    {
+        int best = LevelManagerProxy.Get ().GetCurrentLevelScore ();
+        m_BestNumber.text = best > 0 ? best.ToString() : "";
+        m_LevelName.text = LevelManagerProxy.Get ().GetCurrentLevelName ();
+    }
 
     private void Update ()
     {
-        m_CommandNumber.text = "Command Number: " + CommandStackProxy.Get ().GetNumberOfCommand ();
-        m_BinNumber.text = "Bin Number: " + BinSpawner.ms_BinNumber;
+        m_MoveNumber.text = CommandStackProxy.Get ().GetNumberOfCommand ().ToString();
     }
 
 }
--- LudumDare42/Assets/Scripts/UI/Menu/LevelSelectionMenu.cs
+++ LudumDare42/Assets/Scripts/UI/Menu/LevelSelectionMenu.cs
@@ -5,12 +5,13 @@ using UnityEngine.UI;
 public class LevelSelectionMenu : MonoBehaviour
 {
     [SerializeField] private GameObject m_LevelPrefab;
+    [SerializeField] private Text m_BestNumber;
+    [SerializeField] private Text m_CurrenLevelName;
+
     private Transform[] m_LevelPositions;
-    private Text m_CurrenLevelName;
 
     private void Start ()
     {
-        m_CurrenLevelName = GetComponent<Text> ();
         m_LevelPositions = GameObject.Find ("LevelPositions").GetComponentsInChildren<Transform> ().SubArray (1);
         Dictionary<int, string> levelIdToNames = LevelManagerProxy.Get ().GetLevelNames ();
         foreach (int id in levelIdToNames.Keys)
@@ -22,7 +23,9 @@ public class LevelSelectionMenu : MonoBehaviour
 
     private void Update ()
     {
-        m_CurrenLevelName.text = LevelManagerProxy.Get ().GetCurrentLevelName () + " Best: " + LevelManagerProxy.Get ().GetCurrentLevelScore ();
+        int best = LevelManagerProxy.Get ().GetCurrentLevelScore ();
+        m_BestNumber.text = best > 0 ? best.ToString () : "";
+        m_CurrenLevelName.text = LevelManagerProxy.Get ().GetCurrentLevelName ();
         if (Input.GetKeyDown (KeyCode.Return) || Input.GetKeyDown (KeyCode.Space))
         {
             new GameFlowEvent (EGameFlowAction.Start).Push ();
